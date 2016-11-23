1b1758fca2e310b3cf3833720a935e98673682a0
etsai2 <etsai2@gmail.com>
2016-11-23 15:27:08 -0800

    Setup module for sensor fusion

---
+++ sensorfusion/src/main/java/com/mbientlab/tutorial/sensorfusion/ScannerActivity.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2014-2015 MbientLab Inc. All rights reserved.
+ *
+ * IMPORTANT: Your use of this Software is limited to those specific rights granted under the terms of a software
+ * license agreement between the user who downloaded the software, his/her employer (which must be your
+ * employer) and MbientLab Inc, (the "License").  You may not use this Software unless you agree to abide by the
+ * terms of the License which can be found at www.mbientlab.com/terms.  The License limits your use, and you
+ * acknowledge, that the Software may be modified, copied, and distributed when used in conjunction with an
+ * MbientLab Inc, product.  Other than for the foregoing purpose, you may not use, reproduce, copy, prepare
+ * derivative works of, modify, distribute, perform, display or sell this Software and/or its documentation for any
+ * purpose.
+ *
+ * YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+ * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL MBIENTLAB OR ITS LICENSORS BE LIABLE OR
+ * OBLIGATED UNDER CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER LEGAL EQUITABLE
+ * THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT,
+ * PUNITIVE OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY,
+ * SERVICES, OR ANY CLAIMS BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *
+ * Should you have any questions regarding your right to use this Software, contact MbientLab via email:
+ * hello@mbientlab.com.
+ */
+
+package com.mbientlab.tutorial.sensorfusion;
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.mbientlab.bletoolbox.scanner.BleScannerFragment;
+import com.mbientlab.bletoolbox.scanner.BleScannerFragment.ScannerCommunicationBus;
+import com.mbientlab.metawear.MetaWearBoard;
+import com.mbientlab.metawear.android.BtleService;
+import com.mbientlab.metawear.module.SensorFusion;
+
+import java.util.UUID;
+
+import bolts.Task;
+
+public class ScannerActivity extends AppCompatActivity implements ScannerCommunicationBus, ServiceConnection {
+    public static final int REQUEST_START_APP= 1;
+
+    private BtleService.LocalBinder serviceBinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getApplicationContext().bindService(new Intent(this, BtleService.class), this, BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        ///< Unbind the service when the activity is destroyed
+        getApplicationContext().unbindService(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case REQUEST_START_APP:
+                ((BleScannerFragment) getFragmentManager().findFragmentById(R.id.ble_scanner_fragment)).startBleScan();
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public UUID[] getFilterServiceUuids() {
+        return new UUID[] {MetaWearBoard.METAWEAR_SERVICE_UUID};
+    }
+
+    @Override
+    public long getScanDuration() {
+        return 10000L;
+    }
+
+    @Override
+    public void onDeviceSelected(BluetoothDevice device) {
+        final MetaWearBoard mwBoard= serviceBinder.getMetaWearBoard(device);
+
+        final ProgressDialog connectDialog = new ProgressDialog(this);
+        connectDialog.setTitle(getString(R.string.title_connecting));
+        connectDialog.setMessage(getString(R.string.message_wait));
+        connectDialog.setCancelable(false);
+        connectDialog.setCanceledOnTouchOutside(false);
+        connectDialog.setIndeterminate(true);
+        connectDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), (dialogInterface, i) -> {
+            mwBoard.disconnectAsync();
+        });
+        connectDialog.show();
+
+        mwBoard.connectAsync()
+                .continueWithTask(task -> {
+                    if (task.isCancelled()) {
+                        return task;
+                    }
+                    return task.isFaulted() ? reconnect(mwBoard) : Task.forResult(null);
+                })
+                .continueWith(task -> {
+                    if (!task.isCancelled()) {
+                        connectDialog.dismiss();
+
+                        if (mwBoard.getModule(SensorFusion.class) == null) {
+                            mwBoard.disconnectAsync();
+                            new AlertDialog.Builder(ScannerActivity.this)
+                                    .setCancelable(false)
+                                    .setTitle(R.string.dialog_title_error)
+                                    .setMessage(R.string.message_sensor_fusion)
+                                    .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                                        ((BleScannerFragment) getFragmentManager().findFragmentById(R.id.ble_scanner_fragment)).startBleScan();
+                                    })
+                                    .show();
+                        } else {
+                            /*
+                            Intent navActivityIntent = new Intent(ScannerActivity.this, DeviceSetupActivity.class);
+                            navActivityIntent.putExtra(DeviceSetupActivity.EXTRA_BT_DEVICE, device);
+                            startActivityForResult(navActivityIntent, REQUEST_START_APP);
+                            */
+                        }
+                    }
+
+                    return null;
+                }, Task.UI_THREAD_EXECUTOR);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        serviceBinder = (BtleService.LocalBinder) service;
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+
+    }
+
+    public static Task<Void> reconnect(final MetaWearBoard board) {
+        return board.connectAsync()
+                .continueWithTask(task -> {
+                    if (task.isFaulted()) {
+                        return reconnect(board);
+                    } else if (task.isCancelled()) {
+                        return task;
+                    }
+                    return Task.forResult(null);
+                });
+    }
+}
